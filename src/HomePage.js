2713d756099064b238393502500f8377d482dbd2
Shichao Wang <swang206@myseneca.ca>
2020-03-08 04:27:37 -0400

    文本更新

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,7 +9,9 @@ class HomePage extends React.Component{
                 <p>通过使用React Route实现SPA - Single Page Application</p>
                 <p>
                     通过SPA可以实现切换页面时音乐播放不中断！<br/>
-                    屏幕过窄的时候播放器显示不正确，需要修正
+                    屏幕过窄的时候播放器显示不正确，需要修正 <br/>
+                    播放器进度条还不能拖动，可以试试做一个 <br/>
+                    接下来要做的：播放列表、播放模式、云端存储歌曲、打造成我的云端播放器！！
                 </p>
             </div>
         )
