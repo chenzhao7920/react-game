0832837cc1f3e82d37ee4455772d354d3d997258
赵晨 <zhaochen@zhaochendeMacBook-Pro.local>
2020-08-07 15:30:53 -0400

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,8 +28,8 @@ class PageHeader extends React.Component {
                                     Game-Projects
                                </a>
                                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <NavLink to="/game1" className="dropdown-item">React - Knight Tour</NavLink>
-                                    <NavLink to="/game2" className="dropdown-item">React Native - Knight Tour</NavLink>
+                                    <NavLink to="/game1" className="dropdown-item">React - Knight's Tour</NavLink>
+                                    <NavLink to="/game2" className="dropdown-item">React Native - Knight's Tour</NavLink>
                                     <div className="dropdown-divider"></div>
                                     <a className="dropdown-item" href="#">Coming soon</a>
                                 </div>
